2c567925ae6f43dacea3eb05b5590b2f8f834ab0
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-14 08:10:45 +0100

    Render respective components on corresponding navigation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import {
+  ACCOUNT,
+  HOME,
+  LANDING,
+  PASSWORD_FORGET,
+  SIGN_IN,
+  SIGN_UP
+} from '../../constants'
 import Navigation from '../Navigation';
+import { HomePage, Landing, PasswordForgetPage, SigninPage, SignupPage, AccountPage} from '..';
 
 const App = () =>
   <Router>
-    <Navigation />
+    <React.Fragment>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={LANDING} component={Landing} />
+      <Route exact path={HOME} component={HomePage} />
+      <Route exact path={ACCOUNT} component={AccountPage} />
+      <Route exact path={PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route exact path={SIGN_IN} component={SigninPage} />
+      <Route exact path={SIGN_UP} component={SignupPage} />
+    </React.Fragment>
   </Router>
 
 export default App;
