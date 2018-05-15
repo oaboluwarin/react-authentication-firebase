15bfd3a4c0864a451cda1e611da51faa3f87792c
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-15 09:40:17 +0100

    configure firebase authentication

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,1 +1,8 @@
 export { auth } from './firebase';
+export {
+  doCreateUserWithEmailAndPassword,
+  doSignInWithEmailAndPassword,
+  doSignOut,
+  doPasswordReset,
+  doPasswordUpdate,
+} from './auth';
