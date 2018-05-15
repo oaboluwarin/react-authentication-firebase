2420f085c1dd014e8750d2333708e67f7be51b55
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-15 09:41:51 +0100

    rename signup and login files

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as HomePage } from './HomePage';
 export { default as AccountPage } from './AccountPage';
-export { default as SignupPage } from './SignupPage';
-export { default as SigninPage } from './SigninPage';
+export { default as SignupPage } from './Signup';
+export { default as SigninPage } from './Login';
 export { default as PasswordForgetPage } from './PasswordForgetPage';
 export { default as Landing } from './Landing';
